4930206546c2d6b5aeceb6516b9b1046bff45cf6
Diego Anguita M. <tralcan@gmail.com>
2025-07-10 23:26:16 +0000

    tienes que ocupar el modelo gemini-1.5-flash

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -5,5 +5,5 @@ export const ai = genkit({
   plugins: [googleAI({apiKey: process.env.GOOGLE_API_KEY})],
   // This default model is used for text generation.
   // Specific models for images, etc., are defined where they are used.
-  model: 'googleai/gemini-1.5-flash-latest',
+  model: 'googleai/gemini-1.5-flash',
 });
