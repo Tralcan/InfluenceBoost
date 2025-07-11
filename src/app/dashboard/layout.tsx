5bb500ecad549838f00e42261f5e8f81600ab4d9
Diego Anguita M. <tralcan@gmail.com>
2025-07-11 16:17:56 +0000

    quita del menú lateral el icono de la app y el texto InfluenceBoost

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -6,10 +6,7 @@ import {
   SidebarMenu,
   SidebarMenuItem,
   SidebarMenuButton,
-  SidebarInset,
   SidebarFooter,
-  SidebarSeparator,
-  SidebarHeader,
 } from '@/components/ui/sidebar';
 import {
   PlusCircle,
@@ -19,7 +16,6 @@ import {
   Search,
 } from 'lucide-react';
 import Link from 'next/link';
-import { Logo } from '@/components/logo';
 
 export default function DashboardLayout({
   children,
@@ -30,10 +26,6 @@ export default function DashboardLayout({
     <SidebarProvider>
       <div className="flex min-h-screen w-full bg-muted/40">
         <Sidebar>
-            <SidebarHeader>
-              <Logo />
-            </SidebarHeader>
-            <SidebarSeparator />
             <SidebarContent>
               <SidebarMenu>
                 <SidebarMenuItem>
@@ -68,7 +60,7 @@ export default function DashboardLayout({
               </SidebarMenu>
             </SidebarFooter>
         </Sidebar>
-        <div className="flex flex-col flex-1">
+         <div className="flex flex-col flex-1">
           <Header />
           <main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8">
             {children}
